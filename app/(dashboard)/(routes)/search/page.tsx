dd6aa488ca0b5d3a098d0d2993b0c177f0054667
prabhatameher <meher.prabhat1996@gmail.com>
2024-01-24 12:24:55 +0530

    first commit

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>This is a search Page</div>
+    )
+}
+
+export default Search
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Sidebar from "./_components/Sidebar"
+import Navbar from "./_components/Navbar"
+
+const DashbordLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="h-full">
+            <div className="h-[70px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+                <Sidebar />
+            </div>
+            <main className="md:pl-56 h-full pt-[70px]">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default DashbordLayout
